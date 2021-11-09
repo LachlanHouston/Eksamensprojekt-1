5ef75d09ce2be6717b0770c64699d92a19d540dc
Frederikravnborg <frederik@ravnborg.fr>
2021-11-09 17:12:42 +0100

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 """
-Project 1 - xxxx
+Exam Project - Program for grading students
 (02631) Introduction to Programming and Data processing
 By: Lachlan Houston (s214593) og Frederik Ravnborg (s204078)
-Due: xx/xx/xxxx
+Due: 03/12/2021
 """
 
