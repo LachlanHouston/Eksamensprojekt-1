4eaca6b4d9bb343697130f0dcfb4b5fc83b8d3c4
Frederikravnborg <frederik@ravnborg.fr>
2021-11-09 23:10:53 +0100

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -6,47 +6,101 @@ Due: 03/12/2021
 """
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+roundGrades = np.array([-3,0,2,4,7,10,12])
 
 # =============================================================================
 # 1: Round Grade function:
 # =============================================================================
 def roundGrade(grades):
     
-    possibleGrades = np.array([-3,0,2,4,7,10,12])
     gradesRounded = np.zeros(len(grades))
     smallestDifIndex = np.zeros(len(grades))
     for i in range(len(grades)):
         
-        dif = np.abs(possibleGrades - grades[i])
+        dif = np.abs(roundGrades - grades[i])
         smallestDifIndex[i] = dif.argmin()
-        gradesRounded[i] = possibleGrades[int(smallestDifIndex[i])]
+        gradesRounded[i] = int(roundGrades[int(smallestDifIndex[i])])
+        
     return gradesRounded
  
-print(roundGrade(np.array([-2,0,3,5,5,6,6,2.3,6,3,7,9,11,12])))
+# print(roundGrade(np.array([-2,0,3,5,5,6,6,2.3,6,3,7,9,11,12])))
 
 
 
-"""
 # =============================================================================
 # 2: Final Grade function:
 # =============================================================================
 def computeFinalGrades(grades):
+
+    pdGrades = pd.read_csv(grades, delimiter=";", header=None)
+    npGrades = np.array(pdGrades)
+    gradesFinal = np.zeros(np.shape(npGrades)[0])
+    notLowest = np.zeros(len(npGrades)-1)
     
-    
-    
+    # For loop that goes through all the rows
+    for i in range (len(npGrades)):
+        
+        if -3 in npGrades[i,:]:
+            gradesFinal[i] = -3
+        
+        if len(npGrades) == 1:
+            
+            gradesFinal[i] = npGrades[i]
+            
+        else:  
+            for j in range(np.shape(npGrades)[1]-1):
+                
+                lowestRemoved = np.zeros(np.shape(npGrades)[1]-1)
+                
+                lowestGrade = np.min(npGrades[i,:])
+                
+                if npGrades[i,j] != lowestGrade:
+                    lowestRemoved[j] = npGrades[i,j]
+                
+            gradesFinal[i] = np.mean(lowestRemoved)
+            
+            gradesFinal = roundGrade(gradesFinal)
+        
+
     return gradesFinal
 
+print(computeFinalGrades("test1.csv"))
 
 
 # =============================================================================
 # 3: Grades Plot function:
 # =============================================================================
-def gradesPlot(grades):
+# def gradesPlot(grades):
+
+
+plotData = computeFinalGrades("test1.csv")
+l = np.zeros(len(roundGrades))
+for i in range(len(roundGrades)):
+    l[i] = np.sum(plotData == roundGrades[i])
     
 
+# Designing and running the plot
+colors = ["r","g","b","m","c","peru","yellow"]
+for i in range(len(roundGrades)):
+    plt.bar(str(roundGrades[i]), l[i], color=colors[i])
+
+plt.title("Final grades")
+plt.xlabel("Grade on the 7-step scale")
+plt.ylabel("Number of students")
+plt.xlim([-0.5, 7])
+plt.ylim([0, np.max(l)+0.3])
+plt.show()
+
+
+
+
+#     return
+
 
 
 # =============================================================================
-# 4: Main Script:
+# 4: Main Script
 # =============================================================================
-"""
