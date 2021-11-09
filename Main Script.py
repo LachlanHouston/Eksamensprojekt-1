6c0de3aa5e0b1e7d080bf85ee03592154a0b08d1
Lachlan Houston <LachlanHouston111@gmail.com>
2021-11-09 16:03:30 +0100

    Add files via upload

---
+++ Main Script.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Project 1 - xxxx
+(02631) Introduction to Programming and Data processing
+By: Lachlan Houston (s214593) og Frederik Ravnborg (s204078)
+Due: xx/xx/xxxx
+"""
+
