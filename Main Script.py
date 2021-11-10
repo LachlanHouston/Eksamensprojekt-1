d7533d90788e69361db1c3e6fa396967c8a81538
Lachlan Houston <LachlanHouston111@gmail.com>
2021-11-10 13:22:02 +0100

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -1,8 +1,38 @@
-# -*- coding: utf-8 -*-
 """
 Project 1 - xxxx
 (02631) Introduction to Programming and Data processing
 By: Lachlan Houston (s214593) og Frederik Ravnborg (s204078)
 Due: xx/xx/xxxx
 """
+import numpy as np
+import pandas as pd
 
+# =============================================================================
+# roundGrade Function:
+# =============================================================================
+def roundGrade(grades):
+    # Insert your code here
+    gradesRounded = np.array([-3,0,2,4,7,10,12])
+    return gradesRounded
+
+# =============================================================================
+# computeFinalGrades:
+# =============================================================================
+def computeFinalGrades(grades):
+    # Insert your code here
+    gradesFinal = np.zeros(len(grades))
+    return gradesFinal
+
+# =============================================================================
+# gradesPlot:
+# =============================================================================
+def gradesPlot(grades):
+    # Insert your code here
+    None
+    
+# =============================================================================
+# Main Script:
+# =============================================================================
+while (True):
+    None
+    break
