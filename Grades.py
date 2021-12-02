e5911f7c090ecead0dc047bcd06397601a1a7335
Lachlan Houston <LachlanHouston111@gmail.com>
2021-12-02 13:33:19 +0100

    Update Grades.py

--- Grades.py
+++ Grades.py
@@ -284,7 +284,9 @@ while True:
 # =============================================================================
 # Checking/Removal of Errors
     # Lachlan John Houston (s214593)
-
+    
+# A sequence that checks the data for incorrect assignments (assignments that dont fit into the 7 grade scale, and identitical studentIDs)
+# Afterwards asks if user wishes to delete errornous data
 # =============================================================================
         
     # User can see errors in data file, and can choose whether to delete data with error or not
@@ -395,7 +397,8 @@ while True:
 # =============================================================================
 # Grades List
     # Frederik Ravnborg
-#
+
+# The list of students and their grades, and their calculated final grades is printed here. A simple sorting is done to sort students alphabetically (namewise)
 # =============================================================================
     
     # User is shown a list over grades alphabetically sorted, and with final grade given
